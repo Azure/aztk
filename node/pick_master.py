d013cce697353d77ef0e694a6d92d34e4ed4bb86
Timothee Guerin <tiguerin@microsoft.com>
2017-06-22 09:16:31 -0700

    fix style

--- node/pick_master.py
+++ node/pick_master.py
@@ -14,14 +14,12 @@ account_name = os.environ["AZ_BATCH_ACCOUNT_NAME"]
 account_key = os.environ["ACCOUNT_KEY"]
 account_url = os.environ["ACCOUNT_URL"]
 
-# pool_id = "sparktest"
-# node_id = "abc"
-
 print("Pool id is", node_id)
 print("Node id is", node_id)
 print("Account name", account_name)
 
 master_node_metadata_key = "_spark_master_node"
+
 
 def get_client():
 
@@ -30,26 +28,28 @@ def get_client():
         account_key)
     return batch.BatchServiceClient(credentials, base_url=account_url)
 
+
 def get_master_node_id(pool: batchmodels.CloudPool):
     """
         :returns: the id of the node that is the assigned master of this pool
     """
-    if(pool.metadata is None):
+    if pool.metadata is None:
         return None
 
     for metadata in pool.metadata:
         if metadata.name == master_node_metadata_key:
             return metadata.value
-    
+
     return None
 
 
 def try_assign_self_as_master(client: batch.BatchServiceClient, pool: batchmodels.CloudPool):
     currentMetadata = pool.metadata or []
-    newMetadata = currentMetadata + [{"name": master_node_metadata_key, "value": node_id}]
+    newMetadata = currentMetadata + \
+        [{"name": master_node_metadata_key, "value": node_id}]
 
-    try: 
-        client.pool.patch(pool_id,  batchmodels.PoolPatchParameter(
+    try:
+        client.pool.patch(pool_id, batchmodels.PoolPatchParameter(
             metadata=newMetadata
         ), batchmodels.PoolPatchOptions(
             if_match=pool.e_tag,
@@ -57,26 +57,28 @@ def try_assign_self_as_master(client: batch.BatchServiceClient, pool: batchmodel
         return True
     except batcherror.BatchErrorException:
         print("Failed to gain power!")
-        return False 
+        return False
 
-def find_master(client: batch.BatchServiceClient): 
-    for i in range(0, 5): 
-        pool: batchmodels.CloudPool = client.pool.get(pool_id)
+
+def find_master(client: batch.BatchServiceClient):
+    for i in range(0, 5):
+        pool = client.pool.get(pool_id)
         master = get_master_node_id(pool)
 
         if master:
             print("Pool already has a master '%s'. This node will be a worker" % master)
+            return True
         else:
             print("Pool has no master. Fighting for the throne! (%i/5)" % (i + 1))
             result = try_assign_self_as_master(client, pool)
 
-            if result: 
+            if result:
                 print("The battle has been won! Node %s is the new master.", node_id)
                 return True
-            
 
     print("Unable to assign node as a master in 5 tries")
     return False
+
 
 def run():
     client = get_client()
