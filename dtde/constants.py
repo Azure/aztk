6d73ede1c0369483b289b235966f55a6a5b8171c
Timothee Guerin <timothee.guerin@outlook.com>
2017-07-06 13:52:55 -0700

    Created shared config and azure api

--- dtde/constants.py
+++ dtde/constants.py
@@ -1,3 +1,8 @@
+import os
+
 _MASTER_UI_PORT = 8082
 _WEBUI_PORT = 4040
 _JUPYTER_PORT = 7777
+
+ROOT_PATH = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+CONFIG_PATH = os.path.join(ROOT_PATH, 'configuration.cfg') 
