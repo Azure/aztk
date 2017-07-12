8b45359ff6fe13995c96661d69c9691c509bf33c
Timothee Guerin <tiguerin@microsoft.com>
2017-07-12 10:52:16 -0700

    Jupyter working

--- node_scripts/install/spark.py
+++ node_scripts/install/spark.py
@@ -58,7 +58,7 @@ def setup_connection():
 
     master_file = open(os.path.join(spark_conf_folder, "master"), 'w')
 
-    print("Adding master node ip to conf file" % (master_node_ip))
+    print("Adding master node ip to conf fil '%s'" % master_node_ip)
     master_file.write("%s\n" % master_node_ip)
 
     master_file.close()
@@ -90,38 +90,41 @@ def setup_jupyter():
     print("Setting up jupyter.")
     call(["/anaconda/envs/py35/bin/jupyter", "notebook", "--generate-config"])
 
-    jupyter_config_file = os.path.join(os.path.expanduser("~"), ".jupyter/jupyter_notebook_config.py")
+    jupyter_config_file = os.path.join(os.path.expanduser(
+        "~"), ".jupyter/jupyter_notebook_config.py")
 
     with open(jupyter_config_file, "a") as config_file:
         config_file.write('\n')
         config_file.write('c.NotebookApp.token=""\n')
         config_file.write('c.NotebookApp.password=""\n')
     shutil.rmtree('/usr/local/share/jupyter/kernels')
-    os.makedirs('/usr/local/share/jupyter/kernels/pyspark', exist_ok = True)
+    os.makedirs('/usr/local/share/jupyter/kernels/pyspark', exist_ok=True)
 
     with open('/usr/local/share/jupyter/kernels/pyspark/kernel.json', 'w') as outfile:
-        data=generate_jupyter_config()
+        data = generate_jupyter_config()
         json.dump(data, outfile, indent=2)
 
 
 def start_jupyter():
-    jupyter_port=config.jupyter_port
+    jupyter_port = config.jupyter_port
 
-    my_env=os.environ.copy()
-    my_env["PYSPARK_DRIVER_PYTHON"]="/anaconda/envs/py35/bin/jupyter"
-    my_env["PYSPARK_DRIVER_PYTHON_OPTS"]="notebook --no-browser --port='%s'" % jupyter_port
+    my_env = os.environ.copy()
+    my_env["PYSPARK_DRIVER_PYTHON"] = "/anaconda/envs/py35/bin/jupyter"
+    my_env["PYSPARK_DRIVER_PYTHON_OPTS"] = "notebook --no-browser --port='%s'" % jupyter_port
 
-    Popen(["pyspark"], close_fds = True)
+    print("Starting pyspark")
+    process = Popen(["pyspark"], env=my_env)
+    print("Started pyspark with pid %s" % process.pid)
 
 
 def wait_for_master():
     print("Waiting for master to be ready.")
-    master_node_ip=pick_master.get_master_node_id(
+    master_node_ip = pick_master.get_master_node_id(
         batch_client.pool.get(config.pool_id))
     while True:
-        master_node=get_node(master_node_ip)
+        master_node = get_node(master_node_ip)
 
-        if master_node.state == batchmodels.ComputeNodeState.idle or master_node.state == batchmodels.ComputeNodeState.running:
+        if master_node.state in [batchmodels.ComputeNodeState.idle, batchmodels.ComputeNodeState.running]:
             break
         else:
             print("Still waiting on master")
@@ -129,10 +132,9 @@ def wait_for_master():
 
 
 def start_spark_master():
-    webui_port=config.webui_port
-    master_ip=get_node(config.node_id).ip_address
-    exe=os.path.join(spark_home, "sbin", "start-master.sh")
-    cmd=[exe, "-h", master_ip]
+    master_ip = get_node(config.node_id).ip_address
+    exe = os.path.join(spark_home, "sbin", "start-master.sh")
+    cmd = [exe, "-h", master_ip]
     print("Starting master with '%s'" % " ".join(cmd))
     call(cmd)
 
@@ -142,14 +144,14 @@ def start_spark_master():
 
 def start_spark_worker():
     wait_for_master()
-    exe=os.path.join(spark_home, "sbin", "start-slave.sh")
-    master_node_id=pick_master.get_master_node_id(
+    exe = os.path.join(spark_home, "sbin", "start-slave.sh")
+    master_node_id = pick_master.get_master_node_id(
         batch_client.pool.get(config.pool_id))
-    master_node=get_node(master_node_id)
+    master_node = get_node(master_node_id)
 
-    my_env=os.environ.copy()
-    my_env["SPARK_MASTER_IP"]=master_node.ip_address
+    my_env = os.environ.copy()
+    my_env["SPARK_MASTER_IP"] = master_node.ip_address
 
-    cmd=[exe, "spark://%s:7077" % master_node.ip_address]
+    cmd = [exe, "spark://%s:7077" % master_node.ip_address]
     print("Connecting to master with '%s'" % " ".join(cmd))
     call(cmd)
