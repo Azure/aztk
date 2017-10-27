b670d9a85278718d5d041bd710c5fbbb77bb9c09
JS <jiata@microsoft.com>
2017-10-26 23:13:07 -0700

    Update constants.py

--- aztk/constants.py
+++ aztk/constants.py
@@ -5,7 +5,7 @@ import os
 """
 CLI_EXE = 'aztk'
 
-DEFAULT_DOCKER_REPO = "jiata/aztk:0.1.0-spark2.2.0-python3.5.4"
+DEFAULT_DOCKER_REPO = "jiata/aztk-vanilla:0.1.0-spark2.2.0"
 DOCKER_SPARK_CONTAINER_NAME = "spark"
 
 # DOCKER
