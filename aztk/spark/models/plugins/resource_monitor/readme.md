16467c4f20c13d1ccb058cac0bcfb6a008357529
Pablo <paselem@microsoft.com>
2018-04-13 09:06:37 -0700

    remove sudo calls and update default dashboard

---
+++ aztk/spark/models/plugins/resource_monitor/readme.md
@@ -0,0 +1,11 @@
+# Using the Resrouce Monitor Plugin
+
+## Setup
+
+## Visualize data in Grafana
+
+### Importing the default dashboard
+
+## Querying the database
+
+## Data schema
--- aztk/spark/models/plugins/resource_monitor/start_monitor.sh
+++ aztk/spark/models/plugins/resource_monitor/start_monitor.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 cd $DIR
 
 # Install pip requirements
 echo "Install pip requirements  "
-sudo chmod 777 requirements.txt
+#sudo chmod 777 requirements.txt
 pip3 install -r requirements.txt
 
 if  [ "$AZTK_IS_MASTER" = "1" ]; then
@@ -21,6 +21,6 @@ else
 fi
 
 echo "Run nodestats in background"
-sudo touch nodestats.out
-sudo chmod 777 nodestats.out
-sudo python3 nodestats.py > nodestats.out 2>&1 $AZTK_MASTER_IP $AZTK_IS_MASTER $AZ_BATCH_POOL_ID $AZ_BATCH_NODE_ID &
+touch nodestats.out
+#chmod 777 nodestats.out
+python3 nodestats.py > nodestats.out 2>&1 $AZTK_MASTER_IP $AZTK_IS_MASTER $AZ_BATCH_POOL_ID $AZ_BATCH_NODE_ID &
